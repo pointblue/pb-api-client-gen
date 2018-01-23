393090e8c20fc78542a5c8d24a8f45cef118d716
Martin Magana <magana.web@gmail.com>
2018-01-23 10:50:05 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Javascript API Client Generator  
+
+Generate a Javascript API client from a swagger document, using swagger client
+generator.  
+
+This project just makes it easier to define and generate API clients.  
+
+## Installation  
+
+`npm install *package-name-here* --save-dev`  
+
+  - Create an apiClients.yml file
+  - Add command to `package.json` file  
+  
---
+++ generateApiClients.js
@@ -0,0 +1,42 @@
+(function(){
+
+    var FILE_ENCODING = 'UTF-8';
+
+    var fs = require('fs');
+    var YAML = require('yamljs');
+    var CodeGen = require('swagger-js-codegen').CodeGen;
+    var SwaggerParser = require('swagger-parser');
+
+    var clientGenerationOptions = YAML.load('apiClients.yml');
+
+    generateClients();
+
+    function generateClients(){
+        for(var i=0;i<clientGenerationOptions.length;i++){
+            createClientFromOptions( clientGenerationOptions[i] );
+        }
+    }
+
+    function createClientFromOptions( options ){
+        var className = options.name;
+        var moduleName = 'swaggerClient.' + className;
+        var generatedFilePath = options.outputDirectory + moduleName + '.js';
+
+        SwaggerParser.dereference( options.swaggerDefinitionUrl )
+            .then(generateClient)
+            .then(writeClient);
+
+        function generateClient( swaggerDefinition ){
+            return CodeGen.getAngularCode({
+                'moduleName' : moduleName,
+                'className' : className,
+                'swagger' : swaggerDefinition
+            });
+        }
+
+        function writeClient(angularSourceCode){
+            fs.writeFile( generatedFilePath, angularSourceCode, FILE_ENCODING );
+        }
+    }
+
+})();
